fdf8b6143810e4215cf50647b38addddf3157dea
grey <fairingrey@gmail.com>
2019-06-07 15:35:24 -0700

    simplify macro expansion, remove rust-toolchain files also update actix-web to latest in example ignore IDEA files for convenience

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
 #![feature(box_patterns)]
 
 extern crate proc_macro;
+
 use proc_macro::TokenStream;
-use proc_macro2::{Ident, Span, TokenTree};
+use proc_macro2::{Ident, TokenTree};
 use quote::{quote, ToTokens};
 use syn::{
-    parse_macro_input,
-    punctuated::{Pair, Punctuated},
-    token::Comma,
-    FnArg, ReturnType, Type,
+    parse_macro_input, ReturnType, Type,
 };
 
 fn guess_return_type(decl: &Box<syn::FnDecl>) -> Ident {
@@ -51,59 +49,26 @@ fn guess_return_type(decl: &Box<syn::FnDecl>) -> Ident {
     }
 }
 
-fn guess_fn_args(inputs: &Punctuated<FnArg, Comma>) -> proc_macro2::TokenStream {
-    let mut stream = proc_macro2::TokenStream::new();
-
-    for pair in inputs.pairs() {
-        match pair {
-            Pair::Punctuated(_fn_arg, _comma) => {
-                unimplemented!();
-            }
-            Pair::End(fn_arg) => {
-                match fn_arg {
-                    FnArg::Captured(arg) => {
-                        let pat = &arg.pat;
-                        pat.to_owned().to_tokens(&mut stream);
-                        // println!("ARG: {:?}", quote!(#x));
-                    }
-                    _ => {
-                        unimplemented!();
-                    }
-                }
-            }
-        }
-    }
-
-    stream
-}
-
 #[proc_macro_attribute]
 pub fn async_compat(_: TokenStream, handler: TokenStream) -> TokenStream {
     let handler = parse_macro_input!(handler as syn::ItemFn);
 
     let handler_name = &handler.ident;
-    let backup_handler_name = format!("__async_{}", handler.ident.to_string());
-    let backup_handler_name = Ident::new(&backup_handler_name, Span::call_site());
 
     let handler_decl = &handler.decl;
-    let handler_output = &handler_decl.output;
     let handler_block = &handler.block;
     let handler_inputs = &handler_decl.inputs;
     let return_type = guess_return_type(handler_decl);
 
-    let fn_args = guess_fn_args(handler_inputs);
-
     // Build the output, possibly using quasi-quotation
     let expanded = quote! {
         fn #handler_name(#handler_inputs) -> impl Future<Item = #return_type, Error = Error> {
-            #backup_handler_name(#fn_args).boxed().compat()
+            async move #handler_block .boxed().compat()
         }
-
-        async fn #backup_handler_name(#handler_inputs) #handler_output #handler_block
     };
 
     // debug:
-    // println!("{}", expanded);
+    //println!("{}", expanded);
 
     // Hand the output tokens back to the compiler
     TokenStream::from(expanded)
