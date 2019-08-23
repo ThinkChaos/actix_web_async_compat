96da5faf988cd84346656921f66728b44ccfcb32
ThinkChaos <ThinkChaos@users.noreply.github.com>
2019-08-23 11:56:37 +0200

    Cleanup implementation

--- src/lib.rs
+++ src/lib.rs
@@ -12,58 +12,57 @@ use syn::{
 fn guess_return_type(decl: &Box<syn::FnDecl>) -> Ident {
     let output = &decl.output;
 
-    match output {
-        ReturnType::Default => {
-            panic!("return type should be Result");
-        }
-        ReturnType::Type(_, t) => match t {
-            box Type::Path(p) => {
-                let mut tokens = p.path.segments.clone().into_token_stream().into_iter();
+    let mut tokens = match output {
+        ReturnType::Type(_, box Type::Path(p)) => p.path.segments.clone().into_token_stream().into_iter(),
+        _ => panic!("expected return type to be actix_web::Result<_>"),
+    };
 
-                if let Some(token_tree) = tokens.next() {
-                    if let TokenTree::Ident(mut i) = token_tree {
-                        if i == "actix_web" {
-                            for _ in 0..2 {
-                                let token_tree = tokens.next().expect("expected ':'");
-                                if let TokenTree::Punct(p) = token_tree {
-                                    if p.as_char() != ':' {
-                                        panic!("expected return type to be 'actix_web::Result<_>'");
-                                    }
-                                }
-                                else {
-                                    panic!("expected return type to be 'actix_web::Result<_>'");
-                                }
-                            }
-
-                            match tokens.next() {
-                                Some(TokenTree::Ident(i_)) => i = i_,
-                                _ => panic!("expected return type to be 'actix_web::Result<_>'"),
-                            }
-                        }
-                        if i != "Result" {
-                            panic!("should be Result ident");
-                        }
-                    } else {
-                        panic!("should be ident");
+    // actix_web::Result / Result
+    if let Some(TokenTree::Ident(mut i)) = tokens.next() {
+        if i == "actix_web" {
+            for _ in 0..2 {
+                if let Some(TokenTree::Punct(p)) = tokens.next() {
+                    if p.as_char() != ':' {
+                        panic!("expected return type to be 'actix_web::Result<_>'");
                     }
-                };
-
-                tokens.next(); // skip "<"
-
-                if let Some(token_tree) = tokens.next() {
-                    if let TokenTree::Ident(return_type) = token_tree {
-                        return_type
-                    } else {
-                        panic!("should be ident");
-                    }
-                } else {
-                    panic!("must be one more token");
+                }
+                else {
+                    panic!("expected return type to be 'actix_web::Result<_>'");
                 }
             }
-            _ => {
-                panic!("NOT path returned!");
+
+            match tokens.next() {
+                Some(TokenTree::Ident(i_)) => i = i_,
+                _ => panic!("expected return type to be 'actix_web::Result<_>'"),
             }
-        },
+        }
+
+        if i != "Result" {
+            panic!("expected return type to be 'actix_web::Result<_>'");
+        }
+    } else {
+        panic!("expected return type identifier");
+    }
+
+    // <
+    if let Some(TokenTree::Punct(p)) = tokens.next() {
+        if p.as_char() != '<' {
+            panic!("expected '<' token after 'Result'");
+        }
+    }
+    else {
+        panic!("expected '<' token after 'Result'");
+    }
+
+    // Finally, the type inside Result<_>
+    if let Some(token_tree) = tokens.next() {
+        if let TokenTree::Ident(return_type) = token_tree {
+            return_type
+        } else {
+            panic!("expected an identifier after 'Result<'");
+        }
+    } else {
+        panic!("expected actix_web::Result to be generic on return type");
     }
 }
 
@@ -78,7 +77,7 @@ pub fn async_compat(_: TokenStream, handler: TokenStream) -> TokenStream {
     let handler_inputs = &handler_decl.inputs;
     let return_type = guess_return_type(handler_decl);
 
-    // Build the output, possibly using quasi-quotation
+    // Build the output, using quasi-quotation
     let expanded = quote! {
         fn #handler_name(#handler_inputs) -> impl ::futures::Future<Item = #return_type, Error = ::actix_web::Error> {
             async move #handler_block .boxed().compat()
